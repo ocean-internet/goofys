a5048e90f3b933170ab3a267278474adbd405047
Andy Weir <andy@goofys.co.uk>
2024-10-21 09:26:40 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB" lang="en-GB">
+<head>
+    <title>Goofys - Boutique Hostel - Newquay</title>
+    <meta name="description" content="Take a blend of facilities, from Hotel, Bed &amp; Breakfast, Surf Lodge, Hostel, together with one of Newquay’s most prominent and popular locations, and you get a Boutique Hostel - Goofys.">
+    <meta name="verify-v1" content="yz7awLwlLycCFWLhmysUl6KSELYjYfweNL5x2LSoGr0=">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <style>
+body {
+    font-family: "Verdana","Arial","Helvetica",sans-serif;
+    color:       #333;
+    text-align:  center;
+    background-color: #fff;
+}
+a {
+    border: 0;
+}
+.container {
+    margin-right: auto;
+    margin-left:  auto;
+    width:        640px;
+    max-width:    90%;
+}
+img.full {
+    max-width: 100%;
+    height:    auto;
+}
+img.social {
+    max-width: 22.5%;
+    height:    auto;
+}
+@media \0screen {
+    img {
+        width: auto; /* for ie 8 */
+    }
+}
+p {
+    margin-left: auto;
+    margin-right: auto;
+    width: 62.5%;
+}
+    </style>
+</head>
+<body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47344838-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+    <div class="container">
+        <img class="full" src="GoofysNewquay.jpg" alt="Goofys Newquay" />
+        <a href="http://facebook.com/GoofysNewquay"><img
+            class="social"
+            src="facebook.png"
+            alt="Facebook"
+            /></a><a href="http://twitter.com/GoofysNewquay"><img
+                class="social"
+                src="twitter.png"
+                alt="Twitter"
+            /></a><a href="http://youtube.com/GoofysNewquay"><img
+                class="social"
+                src="youtube.png"
+                alt="YouTube"
+            /></a><!--<a href="http://www.flickr.com/groups/goofys-newquay/pool/"><img
+                class="social"
+                src="flickr.png"
+                alt="Flickr"
+            />-->
+        <h1>We're Having A Break</h1>
+        <p>We opened Goofys way back in November 2002 - so after 10 years of blood,
+            sweat, tears, fun &amp; frolics - we've decided to take a well earned rest, and have a
+            year off in 2013.</p>
+        <p>We'll be opening our doors again spring 2014, and hope to see you all
+            again then.</p>
+        <p>Andy &amp; Gwen</p>
+        <img class="full" src="KitKat.jpg" alt="KitKat" />
+    </div>
+</body>
+</html>
